e5696d3f2392a54bac4d35bf4e44df3d0ed92de5
moulindavid <moulin.david@live.fr>
2023-05-16 21:40:29 +0200

    feat: update decode strings

--- rust/encode_and_decode_strings_659/src/main.rs
+++ rust/encode_and_decode_strings_659/src/main.rs
@@ -10,17 +10,19 @@ impl Solution {
     }
     pub fn decode(s: &str) -> Vec<String> {
         let mut res = Vec::new();
-        //Keep track of current position in the string.
+        //Keep tracks of current position in the string.
         let mut i = 0;
 
         while i < s.len() {
-            let mut jump = i;
-            while s.chars().nth(jump) != Some('#') {
-                jump += 1;
+            let mut j = i;
+            while s.chars().nth(j) != Some('#') {
+                j += 1;
             }
-            let length = s[i..jump].parse::<usize>().unwrap();
-            res.push(s[jump + 1..jump + 1 + length].to_string());
-            i = jump + 1 + length;
+            //Size of the jump.
+            let length = s[i..j].parse::<usize>().unwrap();
+            res.push(s[j + 1..j + 1 + length].to_string());
+            //Skip each words
+            i = j + 1 + length;
         }
 
         res
@@ -34,7 +36,7 @@ fn main() {
         "tan".to_owned(),
         "ate".to_owned(),
         "nat".to_owned(),
-        "asd3sdas".to_owned()
+        "asdasdas".to_owned()
     ]));
-    dbg!(Solution::decode("3#eat3#tea3#tan3#ate3#nat8#asd3sdas"));
+    dbg!(Solution::decode("3#eat3#tea3#tan3#ate3#nat8#asdasdas"));
 }
