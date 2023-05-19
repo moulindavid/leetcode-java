7cfda70916d56fb5326ecdde77c217647250d9fd
moulindavid <moulin.david@live.fr>
2023-05-19 18:40:45 +0200

    feat: add shorter longest consecutive sequence

--- rust/longest_consecutive_sequence_128/src/main.rs
+++ rust/longest_consecutive_sequence_128/src/main.rs
@@ -3,6 +3,16 @@ use std::collections::HashSet;
 struct Solution;
 
 impl Solution {
+    pub fn longest_consecutive_shorter(nums: Vec<i32>) -> i32 {
+        let nums_set: HashSet<i32> = nums.iter().copied().collect();
+        return nums
+            .into_iter()
+            .filter(|&x| !nums_set.contains(&(x - 1)))
+            .map(|x| (x..).take_while(|x| nums_set.contains(x)).count())
+            .max()
+            .map_or(0, |x| x as i32);
+    }
+
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
         let nums_set: HashSet<i32> = nums.into_iter().collect();
         let mut longest = 0;
@@ -18,6 +28,8 @@ impl Solution {
 }
 
 fn main() {
-    dbg!(Solution::longest_consecutive(vec![1, 2, 3]));
-    dbg!(Solution::longest_consecutive(vec![1, 3, 4, 23, 2, 24, 222]));
+    dbg!(Solution::longest_consecutive_shorter(vec![1, 2, 3]));
+    dbg!(Solution::longest_consecutive_shorter(vec![
+        1, 3, 4, 23, 2, 24, 222
+    ]));
 }
